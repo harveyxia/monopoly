bb9f42f5f3c9c72f4ee18f21cbc7f2352443a7c8
Harvey Xia <harvey@harveyxia.com>
2015-12-16 16:04:09 -0500

    fix input parameters

--- test1.py
+++ test1.py
@@ -1,7 +1,11 @@
 from monopoly.monopoly import Monopoly
+from monopoly.board import Board
 from players.basic_player import BasicPlayer
 
+
 def run():
-    players = [BasicPlayer(), BasicPlayer()]
+    board = Board()
+    players = [BasicPlayer(color_index=board.color_index, name="BasicPlayer1"),
+               BasicPlayer(color_index=board.color_index, name="BasicPlayer2")]
     monopoly = Monopoly(players)
     monopoly.run_debug()
