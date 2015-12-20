87453fb51af92d577331819c0942cb0c31032cc4
Charles Jin <cjin32@gmail.com>
2015-12-20 15:52:49 -0500

    fixed the combining - weighing properly

--- simulate.py
+++ simulate.py
@@ -11,14 +11,17 @@ def simulate(turns, games, discount=.05):
         players = [CapRatePlayer(name="NpvPlayer" + str(i), npvs=npvs) for i in xrange(4)]
         monopoly = Monopoly(players=players)
         monopoly.run(100000)
-        npvs = combine_npvs(npvs, monopoly.get_npvs(), i+2)
+        npvs = combine_npvs(npvs, monopoly.get_npvs())
     return npvs
 
-def combine_npvs(prev, current, nGames):
+def combine_npvs(prev, current):
+    num_obs = []
     for i in range(len(prev)):
+        num_obs.append([0] * 7)
         for j in range(6):
             if current[i][1][j] != 0:
-                prev[i][1][j] = (prev[i][1][j]*(nGames-1) + current[i][1][j])/nGames
+                prev[i][1][j] = (prev[i][1][j]*(num_obs[i][j]) + current[i][1][j])/(num_obs[i][j] + 1)
+                num_obs[i][j] += 1
     return prev
 
 def main():
