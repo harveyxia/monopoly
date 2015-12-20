d658a524a332a93222b6237a381903556aff7847
Marvin Qian <fueledbymarvin@gmail.com>
2015-12-19 23:02:48 -0500

    Change npv csv format

--- output.py
+++ output.py
@@ -5,8 +5,27 @@ def npv_distribute(npvs):
 
 def output_npv_file(filename, npvs):
     with open('csv/'+filename, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=['name', 'value'])
+        writer = csv.DictWriter(
+            csvfile,
+            fieldnames=[
+                'name',
+                'value0',
+                'value1',
+                'value2',
+                'value3',
+                'value4',
+                'value5'
+            ])
         writer.writeheader()
         for npv in npvs:
-            for r in npv:
-                writer.writerow({'name': r[0], 'value': r[2]})
+            name = npv[0]
+            values = npv[1]
+            writer.writerow({
+                'name': name,
+                'value0': values[0],
+                'value1': values[0],
+                'value2': values[0],
+                'value3': values[0],
+                'value4': values[0],
+                'value5': values[0]
+            })
