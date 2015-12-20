9f2589ba9d5071c29f44ff5b37fdca94dd89e06e
frankjwu <me@frankjwu.com>
2015-12-20 10:18:01 -0500

    Bump test1

--- test1.py
+++ test1.py
@@ -6,5 +6,5 @@ from players.basic_player import BasicPlayer
 def run():
     players = [BasicPlayer("BasicPlayer1"),
                BasicPlayer("BasicPlayer2")]
-    monopoly = Monopoly(players, max_money=1000)
+    monopoly = Monopoly(players)
     monopoly.run_debug()
