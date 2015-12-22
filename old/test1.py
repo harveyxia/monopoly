9ffcc230ef8b84badb2061cccf6d5f9628c59f18
Charles Jin <cjin32@gmail.com>
2015-12-21 23:59:18 -0500

    added tar ball

--- old/test1.py
+++
@@ -1,9 +0,0 @@
-from monopoly.monopoly import Monopoly
-from players.greedy_player import GreedyPlayer
-
-
-def run():
-    players = [GreedyPlayer("GreedyPlayer1"),
-               GreedyPlayer("GreedyPlayer2")]
-    monopoly = Monopoly(players)
-    monopoly.run(100000)
